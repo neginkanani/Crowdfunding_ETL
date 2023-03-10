97c524fb3a1099a85f069d4e8c7f3a65abad23aa
neginkanani <118688095+neginkanani@users.noreply.github.com>
2023-03-09 21:04:58 -0500

    latest version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Crowdfunding_ETL
+
+This project reads an unclean data and use python to extract data and regular expression to clean the data and finally load the data to a SQL database.
+
+Uncleaned excel data are cleaned and stored in the Resources folder as contacts.csv, campaign.csv, category.csv, and subcategory.csv
+
+A schema used to define the primary and foreign keys is saved as ERD.png
+
+The sql code to create these tables are also saved as crowdfunding_db_query in a postgres database.
---
+++ crowdfunding_db_query.sql
@@ -0,0 +1,65 @@
+drop table "campaign" cascade;
+drop table "contacts" cascade;
+drop table "category" cascade;
+drop table "subcategory" cascade;
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" VARCHAR   NOT NULL,
+    "description" VARCHAR   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" VARCHAR   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" VARCHAR   NOT NULL,
+    "currency" VARCHAR   NOT NULL,
+    "launched_date" VARCHAR   NOT NULL,
+    "end_date" VARCHAR   NOT NULL,
+    "category_id" VARCHAR   NOT NULL,
+    "subcategory_id" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "email" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR   NOT NULL,
+    "category" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR   NOT NULL,
+    "subcategory" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+select * from campaign;
+
+select * from category;
+
+select * from subcategory;
+
+select * from contacts;
+
